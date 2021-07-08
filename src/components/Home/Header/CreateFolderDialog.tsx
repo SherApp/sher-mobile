d771c877fc8b28290bad13f39e3dea92d14b3b2f
Piotr Romanowski <wsrh456@gmail.com>
2021-07-08 17:07:46 +0200

    Styles fixes

--- src/components/Home/Header/CreateFolderDialog.tsx
+++ src/components/Home/Header/CreateFolderDialog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Dialog from 'react-native-dialog';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useApiClient } from '../../../api/useApiClient';
 import { useMutation, useQueryClient } from 'react-query';
 import { useState } from 'react';
@@ -8,6 +8,7 @@ import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 import { useCurrentDirectoryId } from '../useCurrentDirectoryId';
 import useTheme from '../../../theme/useTheme';
+import Typography from '../../misc/Typography';
 
 interface Props {
   onClose(): void;
@@ -49,21 +50,36 @@ const CreateFolderDialog = ({ onClose, visible }: Props) => {
         visible={visible}
         contentStyle={{ backgroundColor: colors.card }}
       >
-        <Dialog.Title>Create folder</Dialog.Title>
-        <Dialog.Input value={folderName} onChangeText={setFolderName} />
+        <Dialog.Title>
+          <Typography>Create folder</Typography>
+        </Dialog.Title>
+        <Dialog.Input
+          value={folderName}
+          onChangeText={setFolderName}
+          placeholder="Folder name"
+          style={styles.font}
+        />
         <Dialog.Button
           label="Cancel"
           onPress={onClose}
           color={colors.primary}
+          style={styles.font}
         />
         <Dialog.Button
           label="Create"
           onPress={handleCreatePress}
           color={colors.primary}
+          style={styles.font}
         />
       </Dialog.Container>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  font: {
+    fontFamily: 'Oswald_400Regular'
+  }
+});
 
 export default CreateFolderDialog;
