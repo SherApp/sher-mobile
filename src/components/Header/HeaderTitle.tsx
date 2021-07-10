3a858c873ac9a53e82bd7d4b66f23f5c88788726
Piotr Romanowski <wsrh456@gmail.com>
2021-07-10 10:03:22 +0200

    HeaderTitle stylish improvements

--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -4,6 +4,8 @@ import { processFontFamily } from 'expo-font';
 import useTheme from '../../theme/useTheme';
 import { StackHeaderTitleProps } from '@react-navigation/stack';
 import { useRoute } from '@react-navigation/native';
+import { View } from 'react-native';
+import { LinearGradient as ExpoLinearGradient } from 'expo-linear-gradient';
 
 interface RouteParams {
   name?: string;
@@ -12,27 +14,41 @@ interface RouteParams {
 const HeaderTitle = ({ children }: StackHeaderTitleProps) => {
   const { params } = useRoute();
 
-  const { spacing, gradients } = useTheme();
+  const { gradients } = useTheme();
 
   return (
-    <Svg height="100" width="200" style={{ marginLeft: spacing(1) }}>
-      <Text
-        fill="url(#a)"
-        fontSize="24"
-        fontFamily={processFontFamily('Oswald_400Regular')!}
-        textAnchor="start"
-        y="50%"
-        dy="0.3em"
-      >
-        {(params as RouteParams)?.name ?? children}
-      </Text>
-      <Defs>
-        <LinearGradient id="a" x1="0" y1="0" x2="1" y2="0">
-          <Stop offset="0" stopColor={gradients.primary[0]} />
-          <Stop offset="1" stopColor={gradients.primary[1]} />
-        </LinearGradient>
-      </Defs>
-    </Svg>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        overflow: 'hidden'
+      }}
+    >
+      <Svg height="50" width="200" style={{ flexGrow: 1 }}>
+        <Text
+          fill="url(#a)"
+          fontSize="24"
+          fontFamily={processFontFamily('Oswald_400Regular')!}
+          textAnchor="start"
+          y="50%"
+          dy="0.3em"
+        >
+          {(params as RouteParams)?.name ?? children}
+        </Text>
+        <Defs>
+          <LinearGradient id="a" x1="0" y1="0" x2="1" y2="0">
+            <Stop offset="0" stopColor={gradients.primary[0]} />
+            <Stop offset="1" stopColor={gradients.primary[1]} />
+          </LinearGradient>
+        </Defs>
+      </Svg>
+      <ExpoLinearGradient
+        colors={['transparent', 'white']}
+        start={[0, 0]}
+        end={[1, 0]}
+        style={{ height: 50, width: 25, transform: [{ translateX: -25 }] }}
+      />
+    </View>
   );
 };
 
